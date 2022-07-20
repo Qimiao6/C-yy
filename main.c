b1211f1074e8a7eb53d3895433b561743aff7101
诸葛斌 <108967191+Qimiao6@users.noreply.github.com>
2022-07-20 23:28:58 +0800

    循环分支语句
    循环分支语句

--- main.c
+++ main.c
@@ -1,197 +1,292 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//                      三目操作符
+//#define mm 100  //#define定义的表示符常量
+//#define xx 10     //注意不要用双引号
 //#include<stdio.h>
 //int main()
 //{
-//	int i = 7;
-//	int b = 77;
-//	int m = i > b ?i:b;
-//	printf("%d\n", m);
-//	printf("%d\n",i < b ? i : b);
+//	
+//	int vv[xx] = { 0 };
+//	printf("%d", mm);
 //
-//	return 0;
-//}
-
-
+//	}
+///                              if语句
 //#include<stdio.h>
 //int main()
 //{
 //	int i = 0;
-//	int d = 0;
-//	scanf("%d%d", &i, &d);
-//	printf("%d\n", (i > d) ? i : d);
+//	scanf("%d", &i);
+//	if (i <= 18)
+//		printf("青年");
+//	else if (i > 18 && i < 35)
+//		printf("中年");
+//	else
+//
+//		printf("老年");
 //	return 0;
 //	}
-//
-//               逻辑与
-//#include<stdio.h>
+//#include <stdio.h>
 //int main()
 //{
-//	int i = 18;
-//	int c = 1;
-//	if (i && c)
+//	int i = 1;
+//	while(i<=100)
 //	{
-//		printf("hehe");
-//		
-//	}
-//	return 0;
-//	}
-
-//#include<stdio.h>
-//int main()
-//{
-//	int i = 18;
-//	int c = 1;
-//	if (i >= 18 && c < 15)//逻辑与，只有两边都为真才执行
-//	{
-//		printf("姐姐");
-//
+//		if (i % 2 == 1)
+//			printf("%d\n",i);		
+//		i++;
 //	}
 //	return 0;
 //}
-//                          逻辑或
-//
+
+        ///                             循环分支
 //#include<stdio.h>
 //int main()
 //{
 //	int i = 1;
-//	int c = 0;
-//	if (i || c)//逻辑或，只要有一个为真就会执行
+//	int y = 2;
+//	if (0 == i)//       2  if判断为假他的下段程序不执行，或者代码块不执行
+//		if (2 == y)//   上面为假不执行
+//			printf("hehe");
+//		else//           不执行
+//			printf("haha");
+//	return 0;//  1   啥都不会输出
+//}
+
+//                          
+//                          return
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int h = 0;
+//	if (0 == i)
 //	{
 //		printf("hehe");
-//
+//		return 0;//函数中只要执行了return,将直接结束函数。
+//		//         循环的话是直接跳到第一句判断
 //	}
+//	if (0 == h)
+//		printf("haha");
 //	return 0;
 //}
-//#include<stdio.h>
-//int main()
-//{
-//	auto int a = 3;//auto是自动的意思，局部变量都是自动创建自动销毁的
-//	//现在可以不用加了，一前需要罢了
-//	int b = 4;
-//	int c = 1;
-//	int d =(c=a + b, a=c + b, a=c + a);//由左往右计算，输出最右值
-//	//      c=7;   a=7+4=11   a=7+11=18
-//	printf("%d\n", d);
-//	return 0;
-//
-//	
-//}
-//                   类型重定义
-//
-//#include<stdio.h>
-//typedef unsigned int cco;//把unsigned int的名字改为cco，本质没变还是无符号整形
-//int main()
-//{
-//	unsigned int i = 9;//无符号整数类型i=9，定义比较繁琐
-//	cco i1 = 10;//与上一行类型一样
-//	printf("%d\n", i1);//输出10，i1为整数类型
-//	return  0;
-//
-//	}
-
-//						寄存器关键字
+//                    判断输入值的奇数偶数
 //#include <stdio.h>
 //int main()
 //{
-//	register int o = 10;//register是寄存器关键字，加在类型前面
-//	//加了register后，表示建议把 o 变量放到寄存器当中，只是建议
-//	//寄存器中运行速度比较快
-//	return 0;
-//}
-//                                 调用函数
-// 
-// 
-// //                          static的使用
-
-//修饰局部变量
-
-//#include<stdio.h>
-//void bibi()
-//{
-//	static int a = 1;//加了static之后，变量只有main结束之后才会初始化
-//	//int a = 1;
-//	a++;
-//	printf("%d\n", a);
-//	}
-//int main()
-//{
-//	int j = 0;
-//	while (j < 10)
-//	{
-//		bibi();//调用bibi函数
-//		j++;
-//	}
-//	return 0;
-//}
-
-//修饰全局变量
-//#include<stdio.h>
-//int main()
-//{
-//	extern int bb;
-//	//extern int kk;//kk（全局变量）被static修饰后将失去外部链接属性
-//
-//	printf("%d\n", bb);
-//	//printf("%d\n", kk);
-//
-//	return 0;
-//	}
-
-
-//修饰函数
-//#include<stdio.h>
-//int main()
-//{
-//	int dd = 3;
-//	int ss = 7;
-//	extern int Add();//Add被加static后，将失去外部链接属性；
-//	int v = Add(dd, ss);
-//	printf("%d\n", v);
-//	return 0;
-//}
-// 
-// 
-//小乐乐学校教学楼的电梯前排了很多人，他的前面有n个人在等电梯。
-//电梯每次可以乘坐12人，每次上下需要的时间为4分钟（上需要2分钟，下需要2分钟）
-//。请帮助小乐乐计算还需要多少分钟才能乘电梯到达楼上。（假设最初电梯在1层）
-//#include <stdio.h>
-//int main()
-//{
-//    int n = 0;//人数
-//    int i = 12;//运载量
-//    int m = 0;//次数
-//    int j = 0;//时间
-//    scanf("%d", &n);
-//    m = n / i;
-//    printf("%d\n", m * 4 + 2);
-//    return 0;
-//}
-//KiKi写了一个输出“Hello world!”的程序，BoBo老师告诉他printf函数有返回值，
-//你能帮他写个程序输出printf(“Hello world!”)的返回值吗？
-//#include<stdio.h>
-//int main()
-//{
-//    int mm = printf("Hello world!");//注意此处不能加\n，
-//    //因为这样会导致Printf的返回值中加一，应该单独换行
-//    printf("\n");
-//    printf("%d\n", mm);
-//    return 0;
-//}
-
-//      从键盘任意输入一个字符，编程判断是否是字母（包括大小写）。
-//输入描述：
-//输入包括一个字符。
-//输出描述：
-//输出该字符是字母（YES）或不是（NO）。
-//#include <stdio.h>
-//int main()
-//{
-//	char mm = 0;
-//	scanf("%c", &mm);
-//	if (mm >= 'a' && mm <= 'z' || mm >= 'A' && mm <= 'Z')
-//		printf("Yes");
+//	int i = 0;
+//	scanf("%d", &i);
+//	if (i % 2 == 1)
+//		printf("奇数");
 //	else
-//		printf("No");
+//		printf("偶数");
+//
+//	return 0;
+//}
+//                            if语句
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	if (1 == i)
+//		printf("星期一");
+//	else if (2 == i)
+//		printf("星期二");
+//	else if (3 == i)
+//		printf("星期三");
+//	else if (4 == i)
+//		printf("星期四");
+//	else if (5 == i)
+//		printf("星期五");
+//	else if (6 == i)
+//		printf("星期六");
+//	else if (7 == i)
+//		printf("星期天");
+//	else 
+//		printf("输入错误");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	switch (i)
+//	{
+//	case 1:
+//		printf("星期一");
+//		break;//停止
+//	case 2:
+//		printf("星期二");
+//		break;
+//	case 3:
+//	printf("星期三");
+//	break;
+//		case 4:
+//	printf("星期四");
+//	break;
+//		case 5:
+//	printf("星期五");
+//	break;
+//		case 6:
+//	printf("星期六");
+//	break;
+//		case 7:
+//	printf("星期天");
+//	break;
+//		default :
+//	printf("输入错误");
+//	break;
+//		}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	switch (i)
+//	{
+//			case 1:
+//			case 2:
+//			case 3:
+//			case 4:
+//			case 5:
+//		printf("工作日");
+//		break;
+//			case 6:
+//			case 7:
+//		printf("休息日");
+//		break;
+//	default:
+//		printf("输入错误");
+//		break;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i <= 10)
+//	{
+//		printf("%d", i);
+//		if (5 == i)
+//			break;//直接结束循环
+//		i++;
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i <= 10)
+//	{
+//		
+//		i++;
+//		if (5 == i)
+//			continue;//终止本轮循环，重新回到判断，
+//		//            下面程序将不执行
+//		printf("%d", i);
+//		
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char i = 0;
+//	while ((i = getchar()) != EOF)//getchar是输入字符
+//	{
+//		putchar(i);//putchar是打印字符
+//	}
 //		return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char i[20] = {0};
+//	char b = 0;
+//	char d = 0;
+//
+//	i = getchar();//getchar是输入字符
+//		putchar(i);//putchar是打印字符
+//		while (d = getchar() != '\n')
+//		{
+//			;
+//		}
+//	printf("打印是否正确a/b\n");
+//	getchar("%c",&b);
+//	if (b == 'a')
+//		printf("正确");
+//	else
+//	printf("错误");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char ii[20] = { 0 };
+//	int d = 0;
+//	printf("请输入密码:>");
+//		scanf("%s", ii);
+		/*printf("您输入的密码是:%s\n", ii);
+		printf("确认密码输入a，取消输入a");*/
+//		while ((d = getchar()) != '\n')
+//		{
+//			;
+//		}
+//		printf("确认密码输入(Y/N)");
+//		int i = getchar();
+//		if (i=='Y')
+//		    printf("已确认");
+//		else
+//			printf("已取消");
+//	return 0;
+//}
+
+//                               for  循环
+
+//先用while做一个人列子
+//#include <stdio.h>
+//int main()
+//{
+//	int mm = 1;//循环初始化部分
+//	while (mm < 10)//循环判断部分
+//	{
+//		printf("%d\n", mm);
+//		mm++;//循环调整部分
+//	}
+//	return 0;
+//}
+//以上可以看出循环由三部分组成
+
+
+//下面开始正式的for语句
+
+//#include <stdio.h>
+//int main()
+//{
+//
+//	for (int i = 1; i <= 10; i++)//不建议把调整变量放下面
+//	{
+//		if (5 == i)
+//			break;   //直接跳出循环执行下一个代码块
+//			continue;//会使代码块结束本轮循环
+//		printf("%d\n", i);
+//	};
+//
+//	return 0;
+//}
+//         for输出0-10
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d",i);
+//	}
+//	return 0;
 //}
