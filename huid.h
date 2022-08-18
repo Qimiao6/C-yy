b2dc58b56440933c9baebaa5ecbabcce9f29dbe9
诸葛斌 <108967191+Qimiao6@users.noreply.github.com>
2022-08-18 23:07:32 +0800

    冒泡函数模拟实现qsort
    冒泡函数模拟实现qsort

---
+++ huid.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<stdio.h>
+//int add(int x, int y);
+//int sub(int x, int y);
+//int rid(int x, int y);
+//int div(int x, int y);
+//void crc(int(*pp)(int, int));
+//void maop(int arr[], int sz);
+void print(int arr[], int sz);
+#include<stdlib.h>
+#include<string.h>
